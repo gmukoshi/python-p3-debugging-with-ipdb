f22fe08b0e4e1e87d470026512ac331df2f40a52
George <mukoshig@gmail.com>
2025-09-27 07:12:06 +0300

    updated_ipdb

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
 import ipdb
 
 def plus_two(num):
-    num + 2
-    return num
+   # ipdb.set_trace()  
+    return num + 2
+
+
+
